aad26ec9c8fc1d1c7a207da37968e686162b6808
jeslinpjames <jeslinpjames007@gmail.com>
2023-11-24 12:22:24 +0530

    Feed Forward Network at the end of the decoder implemented

--- model.py
+++ model.py
@@ -81,12 +81,24 @@ class MultiHeadAttention(nn.Module):
     def forward(self,x):
         return torch.cat([h(x) for h in self.heads],dim =-1)# concat along the channels dimension
 
+class FeedForward(nn.Module):
+    def __init__(self,n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed,n_embed),
+            nn.ReLU(),
+        )
+    def forward(self,x):
+        out =self.net(x)
+        return out
+    
 class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)#Embedding layer
         self.position_embedding_table = nn.Embedding(block_size,n_embed)#position embedding layer
         self.sa_head = MultiHeadAttention(4,n_embed//4)
+        self.ffwd = FeedForward(n_embed)
         self.lm_head = nn.Linear(n_embed,vocab_size)#Linear layer
 
     def forward(self,idx,targets=None):
@@ -95,6 +107,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb = self.position_embedding_table(torch.arange(T,device=device))
         x = pos_emb + tok_emb
         x = self.sa_head(x)
+        x = self.ffwd(x)
         logits =self.lm_head(x)#(B,T,Vocab_size)
         if targets is None:
             loss = None
